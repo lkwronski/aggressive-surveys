ac3f84bc77e4851d1138c52f466e69a3a9303f3b
Łukasz Wroński <lukasz.wronski@comarch.com>
2020-04-21 20:48:26 +0200

    Remove comment

--- src/main/java/com/agh/surveys/service/answer/IAnswerService.java
+++ src/main/java/com/agh/surveys/service/answer/IAnswerService.java
@@ -7,15 +7,12 @@ import java.util.List;
 
 public interface IAnswerService {
 
-    List<Answer> findAll(Long questionId); //returns all answers for a question
+    List<Answer> findAll(Long questionId);
 
-    Answer addAnswer(Long questionID, String userID, AnswerDetails answerDetails); //adds answer for a question
+    Answer addAnswer(Long questionID, String userID, AnswerDetails answerDetails);
 
-    Answer getAnswer(Long AnswerId); //get answer for a question
+    Answer getAnswer(Long AnswerId);
 
-    void deleteAnswer(Long answerId); //delete answer <we need questionId? ~probably not>
+    void deleteAnswer(Long answerId);
 
-    //TODO dodać opcję put
-
-//    List<Answer> addAllAnswerDetails(List<AnswerDetails> answerDetails);
 }
