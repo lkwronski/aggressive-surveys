e1f0e6097dbb4e09f712ae98b0d35bb95243f4d1
lukasz.kowalski <lukowalski97@gmail.com>
2020-04-28 16:33:48 +0200

    User based groups API endpoint added

--- src/main/java/com/agh/surveys/model/group/dto/GroupRespDto.java
+++ src/main/java/com/agh/surveys/model/group/dto/GroupRespDto.java
@@ -11,11 +11,13 @@ import java.util.stream.Collectors;
 @Data
 @NoArgsConstructor
 public class GroupRespDto {
+    private Integer groupId;
     private String leaderNick;
     private String groupName;
     private List<String> groupMembersNicks;
 
     public GroupRespDto(Group group){
+        this.groupId = group.getId();
         this.leaderNick= group.getGroupLeader().getUserNick();
         this.groupName=group.getGroupName();
         this.groupMembersNicks=group.getGroupMembers()
