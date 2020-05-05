37ba82ecd910c51096212e141c3abc794e4a4e41
Aleksander Stando <alek.stando@gmail.com>
2020-05-05 21:48:27 +0200

    Connected sending answer with backend

--- ui/src/app/answer-poll/answer-poll.page.ts
+++ ui/src/app/answer-poll/answer-poll.page.ts
@@ -5,6 +5,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { ServicesService } from '../services/services.service';
 import { GroupService } from '../services/group.service'
 import { PollService } from '../services/poll.service'
+import { AnswerService } from '../services/answer.service';
+import { AnswerBody } from './answerBody';
 
 @Component({
   selector: 'app-answer-poll',
@@ -25,7 +27,8 @@ export class AnswerPollPage implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private services: ServicesService,
-    private pollService: PollService) { }
+    private pollService: PollService,
+    private answerService: AnswerService) { }
 
   ngOnInit() {
     this.groupId = parseInt(this.route.snapshot.paramMap.get('groupId'));
@@ -34,6 +37,8 @@ export class AnswerPollPage implements OnInit {
     this.pollService.findPoll(this.pollId).subscribe(data =>
       this.questions = data.questions)
   }
+
+
 
   logued() {
     this.aut.authState
@@ -69,8 +74,33 @@ export class AnswerPollPage implements OnInit {
     });
   }
 
-  printQuestions(){
-    console.log(this.questions)
+  sendAnswers(){
+    console.log(this.questions);
+    let answers: AnswerBody[] = this.getAnswers();
+    console.log(answers)
+    for(let ans of answers){
+      this.answerService.postAnswer(this.username, ans.questionId, ans.answerDetails).subscribe()
+    }
+  }
+
+  getAnswers(): AnswerBody[]{
+    let answers: AnswerBody[] = []
+    for(var question of this.questions){
+      var answer: AnswerBody = {
+        "answerDetails": {
+          "@type": question.questionDetails.questionType,
+        },
+        "questionId": question.questionId
+      }
+      if(question.questionDetails.questionType === 'TEXT'){
+        answer.answerDetails.answerText = question.answer
+      }
+      if(question.questionDetails.questionType === 'CHECKBOX'){
+        answer.answerDetails.selectedOptions = [question.answer]
+      }
+      answers.push(answer)
+    }
+    return answers
   }
 
 }
